67532712ac90787835e9fe254825d02679b7cfe6
jiaxiu_li <lijiaxiu1020@gmail.com>
2025-04-06 20:39:21 -0400

    change sys

--- bytesep/server.py
+++ bytesep/server.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify, send_file
 # import importlib.util
 # import os
 from bytesep.__main__ import main
-
+import sys
+print(sys.path)
+sys.path.insert(0, '/home/ubuntu/code/music_source_separation/bytesep')
 app = Flask(__name__)
 #
 # def load_main_function():
